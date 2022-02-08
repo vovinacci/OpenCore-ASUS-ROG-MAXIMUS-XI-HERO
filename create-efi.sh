8942d6bc67cebcfb7d7f084e47a1ac3a3769e737
Volodymyr Shcherbinin (vovin) <vovin@lurk.kiev.ua>
2022-02-08 10:50:16 +0200

    Upgrade OpenCore to 0.7.8

--- create-efi.sh
+++ create-efi.sh
@@ -30,12 +30,12 @@ function run-on-trap() {
 }
 
 # Package versions. Set desired versions here.
-readonly OPENCORE_VERSION="0.7.7"
-readonly KEXT_APPLEALC_VERSION="1.6.8"
+readonly OPENCORE_VERSION="0.7.8"
+readonly KEXT_APPLEALC_VERSION="1.6.9"
 readonly KEXT_INTELMAUSI_VERSION="1.0.7"
-readonly KEXT_LILU_VERSION="1.5.9"
+readonly KEXT_LILU_VERSION="1.6.0"
 readonly KEXT_VIRTUALSMC_VERSION="1.2.8"
-readonly KEXT_WHATEVERGREEN_VERSION="1.5.6"
+readonly KEXT_WHATEVERGREEN_VERSION="1.5.7"
 
 # Installation settings
 # Any non-zero value turns on local file copy, instead of downloading.
